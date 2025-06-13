914e556a711526de2d7c0fe70a0f9d9f52978b41
준범 <lnx9988@naver.com>
2025-06-13 15:20:31 +0900

    gradio examples

---
+++ 6_gradio.py
@@ -0,0 +1,17 @@
+import gradio as gr
+
+def add(num1, num2):
+    return num1+num2
+
+interface = gr.Interface(
+    fn = add,
+    inputs = ['number', 'number'],
+    outputs = 'number',
+    title = '계산기',
+    description = '숫자 두 개를 입력하세요',
+    flagging_mode='never'
+
+
+)
+
+interface.launch()
